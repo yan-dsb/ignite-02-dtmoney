3060e22ba6e6b17e503c675c13d4b7f692bc300d
Yan Borges <yandsb16@gmail.com>
2022-10-11 19:29:56 -0400

    transactions table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Summary } from '../Summary';
+import { TransactionTable } from '../TransactionTable';
 import { Container } from './styles';
 
 export function Dashboard(): JSX.Element {
   return (
     <Container>
       <Summary />
+      <TransactionTable />
     </Container>
   );
 }
